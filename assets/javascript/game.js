364c9d4e100b9ba6144260cf61e008233c3abb4f
Angelo Aquino <aaquino909@gmail.com>
2018-09-17 23:56:08 -0700

    i think i'm done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,14 +11,12 @@ var doggie1Num = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
 var doggie2Num = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
 var doggie3Num = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
 var doggie4Num = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-console.log("dog1 " + doggie1Num);
-console.log("dog2 " + doggie2Num);
-console.log("dog3 " + doggie3Num);
-console.log("dog4 " + doggie4Num);
 var doggie1Total = 0;
 var doggie2Total = 0;
 var doggie3Total = 0;
 var doggie4Total = 0;
+var total = 0;
+var counter =0;
 
 function startOver(){
 randomNum =  Math.floor(Math.random() * (120 - 19 + 1)) + 19;
@@ -34,16 +32,16 @@ doggie1Total = 0;
 doggie2Total = 0;
 doggie3Total = 0;
 doggie4Total = 0;
-total = 0;
+var total = 0;
+var counter =0;
+console.log("total " + total );
+console.log("counter " + counter);
+
 $(".totalScore").text("Your total score is: ");
-
 $(".randomNum").text("Random Number: "+ randomNum);
-
 }
 
 function dog(button){
-    var counter = 0;
-
 $(button).on("click", function() {  
     var clickedPic = $(button).attr("alt");  //stores "alt" attribute to clickedPic
 
@@ -71,27 +69,25 @@ if (clickedPic === "dog4"){ //if dog1 = "alt"= dog1 then add to counter
 if (total === randomNum){
     alert("WOOF WOOF! You're a winner!");
     wins ++;
-    console.log(wins);
     $(".wins").text("Wins: " + wins);
+    randomNum =0;
+    counter = 0;
     startOver();
 } else if (total >= randomNum){
     alert("GRRRRR you lost!");
     losses ++;
-    console.log(losses);
     $(".losses").text("Losses: " + losses);
+    randomNum =0;
+    counter = 0;
     startOver();
+    
 }
-
-
 });
 }
+
 dog($(".doggie1"));
 dog($(".doggie2"));
 dog($(".doggie3"));
 dog($(".doggie4"));
-
-
-
-
-
+startOver();
 }); //documentreadyfunction
